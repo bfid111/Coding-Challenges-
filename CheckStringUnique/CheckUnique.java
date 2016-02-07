9f7b014249f5ee56f5f177f3cad56346885997d3
bfid111 <bfid111@gmail.com>
2016-02-06 22:39:35 -0500

    Checks Unique Characters in  A string
    Checks if how many unique characters are there in a given string and
    prints their number of occurances

---
+++ CheckStringUnique/CheckUnique.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+// @Author Youngmin
+// Checks if how many unique characters are there in a given string
+// and prints their number of occurances
+public class CheckUnique {
+	
+	public static void main(String[] args){
+	    int str = countUnique("This is a sample String.");
+		int count[] = countOccurance("This is a sample String.");
+		System.out.println("Number of Unique Characters : " + str);
+		for(int i = 0; i < count.length; i++){
+			if(count[i] > 0){
+				char temp = (char)i;
+				System.out.println(( "\" " + temp + " \"" + " " 
+				     + " count : " + count[i]));
+			}
+		}
+	}
+	static int countUnique(String str){
+		int count = 0; 
+		boolean[] isUnique = new boolean[128]; // There are 0~127 ASCII values 
+		Arrays.fill(isUnique,false);
+		for(int i = 0; i < str.length(); i++){
+			count += isUnique[(int)str.charAt(i)] == false ? 1:0;
+			isUnique[(int)str.charAt(i)] = true; // true after going through the loop
+		}
+		return count;
+	}
+	static int[] countOccurance(String str){
+		int[] strOccurance = new int[128];
+		Arrays.fill(strOccurance, 0);
+		for(int i  = 0; i < str.length(); i++){
+			strOccurance[(int)str.charAt(i)] += 1;
+		}
+		return strOccurance;
+	}
+}
