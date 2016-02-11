bd91f09121ae2096ea85f8236874cacd8d18a3d5
bfid111 <bfid111@gmail.com>
2016-02-11 18:24:23 -0500

    StringPalindrome
    Checks if a given string is a palindrome

--- String Palindrome/StringPalindrome.java
+++ String Palindrome/StringPalindrome.java
@@ -1,12 +1,12 @@
-// Checks if a given string is a permutation
+// Checks if a given string is a Paldindrome
 public class StringPermutation {
 	public static void main(String[] args){
-		boolean s = checkPerm("heeh");
-		boolean d = checkPerm("goodoog");
+		boolean s = checkPaldindrome("heeh");
+		boolean d = checkPaldindrome("goodoog");
 		System.out.println(s);
 		System.out.println(d);
 	}
-	static boolean checkPerm(String str){
+	static boolean checkPaldindrome(String str){
 		for(int i = 0; i < str.length(); i++){
 			if ((str.charAt(i) != str.charAt(str.length()-1-i))){ // if char at position i is not same as char at position i away from the end 
 				return false;
